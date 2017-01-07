563f9ae69ff00edab1ba64b4c1cb3bbe2ce276c1
rakeshgehalot <rakeshgehalotxd@gmail.com>
2017-01-06 21:56:06 -0800

    Add files via upload

---
+++ tools.sh
@@ -0,0 +1,29 @@
+ sudo apt-get install build-essential bison flex gettext texinfo autoconf automake libtool
+ sudo apt-get install openssl
+ sudo apt-get update openssl
+ sudo apt update openssl
+ sudo apt-get openssl
+ sudo apt-get install openssl
+ sudo apt-get install gcc
+ sudo apt-get install make
+ sudo apt-get install makereboot
+ sudo apt-get install ia32-libs
+ sudo apt-get install ia32ncurses5
+ sudo apt-get install ia32z1
+ sudo apt-get install ia32ncursesw5
+ sudo apt-get install iib32ncursesw5
+ sudo apt-get install lib32ncursesw5
+ sudo apt-get install ia32-libs
+ sudo apt-get install lib32ncurses5
+ sudo apt-get install lib32z1
+ sudo apt-get install gawk
+ sudo apt-get install tofrodos
+ sudo apt-get install iproute
+ sudo apt-get install git-core
+ sudo apt-get install net-tools
+ sudo apt-get install ncurses-dev
+ sudo apt-get install ncurses
+ sudo apt-get install zlib1g-dev
+ sudo apt-get install flex
+ sudo apt-get install bison
+
